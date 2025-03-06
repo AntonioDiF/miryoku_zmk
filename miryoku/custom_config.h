b6da2549f541cfe5f00e1e9792c9a53567800143
Antonio Diaz <wisky.brandy@hotmail.com>
2025-03-06 04:15:53 +0000

    Inverting shift and ctrl

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,8 +5,8 @@
 
 #define MIRYOKU_LAYER_GAME \
 &kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             &kp LALT,          \
-&kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           &kp TILDE,         \
-&kp LCTRL,         &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         U_NP,              \
+&kp LCTRL,         &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           &kp TILDE,         \
+&kp LSHFT,         &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         U_NP,              \
 U_NP,              U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP,              U_NP              
 
 #define MIRYOKU_LAYERMAPPING_BASE( \
